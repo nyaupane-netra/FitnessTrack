a57f858fdb6364893524d031363be8ea46867867
Netra Nyaupane <nyaupanenetrap@gmail.com>
2024-09-06 08:14:55 -0500

    Setting up Login page and signup page

--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import styled from "styled-components";
 import LogoImage from "../utils/Images/Logo.png";
@@ -46,15 +46,38 @@ const Right = styled.div`
   justify-content: center;
 `;
 
+const Text = styled.div`
+  font-size: 16px;
+  text-align: center;
+  color: ${({ theme }) => theme.text_secondary};
+  margin-top: 16px;
+  @media (max-width: 400px) {
+    font-size: 14px;
+  }
+`;
+const TextButton = styled.span`
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  transition: all 0.3s ease;
+  font-weight: 600;
+`;
+
 const Authentication = () => {
+  const [login, setLogin] = useState(false);
   return <Container>
     <Left>
         <Logo src={LogoImage}/>
         <Image src={AuthImage}/>
     </Left>
-    <Right>{login ? <>
+    <Right>{!login ? (<>
     <Text>
-       Don't Have an account? </Text></> : <></>}</Right>
+       Don't Have an account? <TextButton onClick={() => setLogin(true)}>SignUp</TextButton></Text>
+       </>
+      ) : (
+      <>
+        <Text>Already have an account? <TextButton onClick={() => setLogin(false)}>SignIn</TextButton></Text>
+      </>)}
+    </Right>
   </Container>
 }
 
